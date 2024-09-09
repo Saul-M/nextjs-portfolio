d6bc5845620424f12bad892f25971a963018c1a6
Saul-M <Saul9874@gmail.com>
2024-09-09 15:11:14 -0700

    Changed the Blog Section

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,22 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Saul Martinez | Portfolio
+        Saul Martinez    |    Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a first-generation college student graduating this December
+        with a Bachelor's in Computer Science and a degree in Cybersecurity. 
+        My technical skills come from school projects where I've developed 
+        software and solved complex problems. Through my role as a Peer Mentor 
+        at the Dream Success Center, which supports undocumented students, 
+        I’ve sharpened my communication and teamwork abilities, working closely 
+        with others in a collaborative setting.
+
+        This blog showcases my projects, skills, and passion for technology 
+        as I seek opportunities to connect with companies that value innovation 
+        and diversity. I'm eager to contribute my expertise while growing as a 
+        developer and cybersecurity specialist.
+`       }  
       </p>
       <div className="my-8">
         <BlogPosts />
